4157c97dd6a4bd9ff405ab728944092d73208b62
almarubin <almarubin10@gmail.com>
2025-10-27 09:48:04 +0200

    next page

--- app/src/main/java/com/example/goout/MainActivity.java
+++ app/src/main/java/com/example/goout/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.goout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        EditText input = findViewById(R.id.input);
+        TextView text = findViewById(R.id.text);
+        Button button  = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(v ->{ ;
+        text.setText(input.getText());
+
+
         });
+        button2.setOnClickListener(view -> {
+            Intent intent  = new Intent( MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+        });
+
     }
+
 }
