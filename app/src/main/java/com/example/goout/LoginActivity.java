061db50124bb3ad1e06c431f1c7187daf5fccff5
tifcohen <tifcohen@gmail.com>
2025-12-10 15:23:47 +0200

    fixed firebase settings

--- app/src/main/java/com/example/goout/LoginActivity.java
+++ app/src/main/java/com/example/goout/LoginActivity.java
@@ -8,31 +8,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.FirebaseApp;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Firebase.initializeApp(this);
+        FirebaseApp.initializeApp(this);
         EditText input = findViewById(R.id.input);
         TextView text = findViewById(R.id.text);
         Button button  = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
         EditText email = findViewById(R.id.email);
 
-        button.setOnClickListener(v ->{ ;
-        text.setText(input.getText());
-
-
-
-
+        button.setOnClickListener(v ->{
+            text.setText(input.getText());
         });
+
         button2.setOnClickListener(view -> {
             Intent intent  = new Intent( LoginActivity.this, SignUpActivity.class);
             startActivity(intent);
         });
-
     }
-
 }
